250d2aef48cb89a0a9e2e44a2d7a27aaa7555029
shivam <shivam.android@gmail.com>
2016-02-29 14:37:38 +0530

    29/2

--- main.cpp
+++ main.cpp
@@ -166,48 +166,6 @@ int findMaxProfit(Job arr[], int n)
     return result;
 }
 
-void KMPSearch(char *pat, char *txt)
-{
-    int M = strlen(pat);
-    int N = strlen(txt);
-
-    // create lps[] that will hold the longest prefix suffix
-    // values for pattern
-    int *lps = (int *)malloc(sizeof(int)*M);
-    int j  = 0;  // index for pat[]
-
-    // Preprocess the pattern (calculate lps[] array)
-    computeLPSArray(pat, M, lps);
-
-    int i = 0;  // index for txt[]
-    while (i < N)
-    {
-        if (pat[j] == txt[i])
-        {
-            j++;
-            i++;
-        }
-
-        if (j == M)
-        {
-            printf("Found pattern at index %d \n", i-j);
-            j = lps[j-1];
-        }
-
-            // mismatch after j matches
-        else if (i < N && pat[j] != txt[i])
-        {
-            // Do not match lps[0..lps[j-1]] characters,
-            // they will match anyway
-            if (j != 0)
-                j = lps[j-1];
-            else
-                i = i+1;
-        }
-    }
-    free(lps); // to avoid memory leak
-}
-
 float power(float x, int y)
 {
     float temp;
